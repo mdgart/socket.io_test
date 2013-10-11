21d16766ce8a1295f5746892663f43d7d3d18e28
mdgart <mauro@eventup.com>
2013-10-11 14:18:53 -0700

    Express Basic App

--- basicserver.js
+++ basicserver.js
@@ -1,4 +1,4 @@
-var io = require('socket.io').listen(8080);
+var io = require('socket.io').listen(80);
 
 io.sockets.on('connection', function (socket) {
     socket.emit('news', { hello: 'world' });
---
+++ expressapp.js
@@ -0,0 +1,20 @@
+var express = require('express')
+    , http = require('http')
+    , app = express()
+    , server = http.createServer(app)
+    , io = require('socket.io').listen(server);
+
+var port = 80;
+if (process.env.NODE_LOCAL_ENV == true) port = 8080;
+server.listen(8080);
+
+app.get('/', function (req, res) {
+   res.sendfile(__dirname + "/index.html");
+});
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
